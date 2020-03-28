a9072749b23b2be11bdddffcf5db59a9f0207885
bioerrorlog <n.iijima.0x01@gmail.com>
2020-03-28 13:33:50 +0900

    example template  commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import Store from "./store";
+import App from "./components/App";
+import "./index.css";
+import { createGlobalStyle } from "styled-components";
+import { PersistGate } from "redux-persist/integration/react";
+
+const { persistor, store } = Store();
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    background-color: #20232a;
+    box-sizing: border-box;
+    transition: all 0.5s ease-in;
+  }
+`;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <GlobalStyle />
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
